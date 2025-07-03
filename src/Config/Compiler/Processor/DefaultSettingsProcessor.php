379cd2c5ef4e6eec2a62f107a9a7bab663549f5f
Roman Nebesnuy <roman@get-code.net>
2025-07-03 17:41:06 +0300

    UIV4-16378

--- src/Config/Compiler/Processor/DefaultSettingsProcessor.php
+++ src/Config/Compiler/Processor/DefaultSettingsProcessor.php
@@ -57,6 +57,7 @@ final class DefaultSettingsProcessor implements ProcessorInterface
                 'net_buffer_length' => 1000000,
                 'no_autocommit' => true,
                 'no_create_info' => false,
+                'no_data' => false,
                 'routines' => false,
                 'single_transaction' => true,
                 'skip_comments' => false,
